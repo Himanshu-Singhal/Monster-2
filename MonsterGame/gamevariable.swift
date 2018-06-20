2d3a9ac77fcf2bd1ce0155376e8731b5fa7d6c5f
User <user@Gowsics-MacBook-Pro.local>
2018-06-19 21:39:57 -0400

    Initial Commit

---
+++ MonsterGame/gamevariable.swift
@@ -0,0 +1,15 @@
+
+
+import Foundation
+
+class gamevariable
+{
+    static var count=0
+    static var player1=0
+    static var player2=0
+    static var flagA=0
+    static var flagB=0
+    static var normalspeed=0
+    static var doublespeed=0
+    
+}
